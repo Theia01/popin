10aed53d5fd10c504ead6abffb8c38d2b1173d6a
maeldebon <mael.dbn@gmail.com>
2019-01-16 11:18:38 +0100

    Adding ; to main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-button = document.querySelector('button')
+button = document.querySelector('button');
 button.addEventListener('click', function () {
 	document.querySelector('div').classList.toggle('show')
-})
+});
