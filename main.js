4930eac426cb8bd98a44e89cb2752483ceae06c6
‘Theia01’ <45165141+Theia01@users.noreply.github.com>
2019-01-16 16:30:38 +0100

    active button

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 
 var div = document.querySelectorAll('div')[1];
-console.log(div)
 var button = document.querySelector('button');
 button.addEventListener('click', function () {	
 	if(div.className == 'hidden'){
@@ -27,11 +26,12 @@ input.addEventListener('click', function(){
 });
 
 document.querySelectorAll('input')[0].addEventListener("keypress", function(){
-
 	var email = document.querySelectorAll('input')[0].value;
 	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	if(re.test(email)){
 		console.log("ok");
 		document.querySelectorAll('input')[1].disabled = ""
-	}})
+		document.querySelectorAll('input')[1].className = "vbtn"
+	}
+	})
 
