8ec14343ed85a71543e55295f7651937b0a0adc5
‘Theia01’ <45165141+Theia01@users.noreply.github.com>
2019-01-16 15:09:44 +0100

    regex

--- main.js
+++ main.js
@@ -19,3 +19,11 @@ var input = document.querySelectorAll('input')[1];
 input.addEventListener('click', function(){
 	document.querySelector('div').className = '';
 });
+
+document.querySelectorAll('input')[0].addEventListener("keypress", function(){
+	var email = document.querySelectorAll('input')[0].value;
+	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	if(re.test(email)){
+		document.querySelectorAll('input')[1].disabled = ""
+	}})
+
