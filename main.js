7b61746e5dd99dd0e3b309c75f8ef15ef825df90
maeldebon <mael.dbn@gmail.com>
2019-01-16 13:10:34 +0100

    Some fix

--- main.js
+++ main.js
@@ -1,21 +1,17 @@
 var button = document.querySelector('button');
 button.addEventListener('click', function () {
-<<<<<<< HEAD
-	document.querySelector('div').classList.toggle('show');
-    document.querySelector('div2').classList.toggle('closed')
-});
-=======
-	document.querySelector('div').classList.toggle('show')
-});
 
+    console.log("Test")
+	document.querySelector('div').classList.toggle('show');
+    document.querySelector('div2').classList.toggle('closed');
+});
 var span = document.querySelector('span');
 span.addEventListener('click', function(){
 	document.querySelector('div').className = '';
-})
+});
 
 document.addEventListener('keydown', function(e){
 	if(e.keyCode == 27){
 		document.querySelector('div').className = '';
 	}
-})
->>>>>>> 6b8124b5933b7dbdd6c08b4ba06b714aaaf97b3c
+});
