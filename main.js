faed737b0c7954aa7ae4087b973dbc60987df1b3
maeldebon <mael.dbn@gmail.com>
2019-01-16 13:06:42 +0100

    test

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var button = document.querySelector('button');
 button.addEventListener('click', function () {
-	document.querySelector('div').classList.toggle('show')
+	document.querySelector('div').classList.toggle('show');
+    document.querySelector('div2').classList.toggle('closed')
 });
