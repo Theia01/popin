abc0647c7aa7bc6b5eeadf358cc8f15722308cd0
‘Theia01’ <45165141+Theia01@users.noreply.github.com>
2019-01-17 11:38:32 +0100

    update

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="FR">
     <head>
         <title>Javascript | PopIn</title>
-        <link rel="icon" href="favicon_pas_ouf_parce_que_cest_celle_des_ops_mais_yavait_que_ca.png" />
+        <link rel="icon" href="image/favicon_pas_ouf_parce_que_cest_celle_des_ops_mais_yavait_que_ca.png" />
         <meta charset="utf-8">
-        <link rel="stylesheet" href="main.css">
+        <link rel="stylesheet" href="css/main.css">
     </head>
     <body>
     <div class="">
@@ -19,7 +19,6 @@
             <input type="submit" class="vbtndbld" name="forminscription" value="S'inscrire" disabled />
         </form>
     </div>
-    <script type="application/javascript" src="main.js"></script>
-
+    <script type="application/javascript" src="script/main.js"></script>
     </body>
 </html>
