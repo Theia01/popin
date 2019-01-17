35024b3117323eefe66fe4e7c70d58675f84d76b
Martinguicheteau <martin.guicheteau2@gmail.com>
2019-01-17 09:05:53 +0100

    regex

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ input.addEventListener('click', function(){
 
 document.querySelectorAll('input')[0].addEventListener("keypress", function(){
 	var email = document.querySelectorAll('input')[0].value;
-	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
 	if(re.test(email)){
 		document.querySelectorAll('input')[1].disabled = ""
 		document.querySelectorAll('input')[1].className = "vbtn"
