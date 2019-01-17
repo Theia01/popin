950b69b796af772a3da41f571e1d713f4d0a7ec5
‘Theia01’ <45165141+Theia01@users.noreply.github.com>
2019-01-17 09:20:02 +0100

    background black

--- main.js
+++ main.js
@@ -5,8 +5,11 @@ var button = document.querySelector('button');
 //quand on click sur le bouton recevoir peluche
 button.addEventListener('click', function () {	
 	if(div.className == 'hidden'){
+		document.querySelectorAll('div')[0].className = "open";
 		div.className = 'show';
+
 	}else{
+		document.querySelectorAll('div')[0].className = "closed";
 		div.className = 'hidden';
 	}
 });
@@ -29,27 +32,33 @@ document.addEventListener('keydown', function(e){
     }
 });
 
+//pour a croix X
 var span = document.querySelector('span');
 span.addEventListener('click', function(){
+	document.querySelectorAll('div')[0].className = "close";
 	div.className = 'hidden';
 });
 
+//pour le bouton echap
 document.addEventListener('keydown', function(e){
 	if(e.keyCode == 27){
+    	document.querySelectorAll('div')[0].className = "close";
 		div.className = 'hidden';
 	}
 });
 
+//quand on valide le bouton submit
 var input = document.querySelectorAll('input')[1];
 input.addEventListener('click', function(){
 	div.className = 'hidden';
 	var p = document.createElement('p');
 	var texte = document.createTextNode('Votre demande a bien été prise en compte');
 	p.appendChild(texte);
-	document.body.append(p)
+	document.body.querySelectorAll('div')[0].append(p)
+	document.querySelectorAll('div')[0].className = "close";
 });
 
-//permet d'activer ou non le bouton selon email
+//permet d'activer ou non le bouton submit selon email
 document.querySelectorAll('input')[0].addEventListener("keypress", function(){
 	var email = document.querySelectorAll('input')[0].value;
 	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
