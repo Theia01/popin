5438c1a20de3f6cc114739737a531f7a32599b29
‘Theia01’ <45165141+Theia01@users.noreply.github.com>
2019-01-17 11:31:45 +0100

    index.php + email.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html lang="FR">
+    <head>
+        <title>Javascript | PopIn</title>
+        <link rel="icon" href="favicon_pas_ouf_parce_que_cest_celle_des_ops_mais_yavait_que_ca.png" />
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="main.css">
+    </head>
+    <body>
+    <div class="">
+        <button class="btn">Recevoir ma peluche</button>
+    </div>
+    <div class="hidden">
+        <span title="Fermer">X</span><br />
+        <h1 class="center">Inscrivez-vous et recevez<br /> votre peluche !</h1><br />
+        <form class="center" method="POST" action=""><br /><br />
+            <label for="mail" class="votreadressemail"><b>Votre adresse mail : </b></label><br /><br />
+            <input type="text" id="mail" name="mail" class="email" /><br><br><br />
+            <input type="submit" class="vbtndbld" name="forminscription" value="S'inscrire" disabled />
+        </form>
+    </div>
+    <script type="application/javascript" src="main.js"></script>
+
+    </body>
+</html>
